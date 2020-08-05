f9f3f68ddaff64d35f9d6d02385c42cc1866e1c5
Randell Dawson <rdawson@onepathtech.com>
2020-08-04 20:56:43 -0700

    fix: change button text on toggle

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,14 @@
 // jquery to hide and toggle the comments until button is clicked
 
+function changeButtonName(button) {
+  var currentBtnText = $(button).text();
+  var buttonAction = currentBtnText.includes('see') ? 'hide' : 'see';
+  $(button).text('Click to ' + buttonAction + ' Comments Slide');
+}
+
 // toggle function
 $('#btn_com').click(function(){
-    $('#randSec').toggle('');
+  $('#randSec').toggle('swing', changeButtonName.bind(null, this));
 });
 
 console.log('Easy to Function')
