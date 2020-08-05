24eac41799dbb52f4a6531f277ac34ad47ce5f7f
Mamun Abdullah <21096193+thetradecoder@users.noreply.github.com>
2020-08-05 11:51:28 +0600

    Update functions.js
    added timing function to display time on the page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,4 +5,16 @@ $('#btn_com').click(function(){
     $('#randSec').toggle('');
 });
 
-console.log('Easy to Function')
+// console.log('Easy to Function')
+
+
+// display time 
+// added by theTradeCoder
+function displayTime() {  
+  setInterval(() => {
+    const date = new Date();
+    $("#date").html(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()},`);
+    $("#time").html(`${date.toLocaleTimeString()}`)
+  }, 1000);
+};
+displayTime();
