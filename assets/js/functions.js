4e2bd68ef3783d244c79a4d1fc760766992075cb
Randell Dawson <rdawson@onepathtech.com>
2020-08-05 08:08:48 -0700

    fix: changed date to use toLocaleDateString

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,16 +11,14 @@ $('#btn_com').click(function(){
   $('#randSec').toggle('swing', changeButtonName.bind(null, this));
 });
 
-// console.log('Easy to Function')
-
 
 // display time 
 // added by theTradeCoder
 function displayTime() {  
   setInterval(() => {
     const date = new Date();
-    $("#date").html(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()},`);
-    $("#time").html(`${date.toLocaleTimeString()}`)
+    $("#date").html(date.toLocaleDateString());
+    $("#time").html(date.toLocaleTimeString());
   }, 1000);
 };
 displayTime();
