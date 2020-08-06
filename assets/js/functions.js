064444d580f00c2e86490abf63a70beb4cdf7077
Mamun Abdullah <21096193+thetradecoder@users.noreply.github.com>
2020-08-07 02:28:15 +0600

    Update functions.js
    Script added for google search option

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -22,3 +22,14 @@ function displayTime() {
   }, 1000);
 };
 displayTime();
+
+// google search option 
+// added by Mamun Abdullah, @theTradeCoder
+$('#googleSearch').submit((e)=>{
+      e.preventDefault();
+      let searchTopic = $('.google-search').val();
+      searchTopic = searchTopic.split(' ').join('+');
+      let searchUrl = `https://www.google.com/search?q=${searchTopic}&oq=${searchTopic}&ie=UTF-8`;
+     window.open(searchUrl);
+    });
+
