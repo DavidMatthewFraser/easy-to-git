a0a07d22c46f7aa7a2d86f68703a1d823defef3b
syntappz <doddsanater87@gmail.com>
2020-08-13 20:51:05 +0100

    abduct spaceship

--- assets/js/challenge2.js
+++ assets/js/challenge2.js
@@ -16,11 +16,16 @@ $(".close-instructions").click(() => popup.hide("ease"));
 $(".png-link").click(() => window.open("https://www.pngguru.com/", "_blank"));
 
 // David's spaceship Logic
-let spaceship = document.getElementById("davids_spaceship");
-
+const spaceship = document.getElementById("davids_spaceship");
+const ufo = $("#biscuit_ufo");
 let timer;
 let x = 300;
 let y = 40;
+let abduct = false;
+
+setTimeout(() => {
+  abduct = true;
+}, 3000);
 
 let spaceshipTimer = () => {
   startTimer();
@@ -28,8 +33,10 @@ let spaceshipTimer = () => {
 
 function startTimer() {
   x++;
-  y--;
+  abduct ? (y -= 1.5) : y--;
+
   updatePostion(x, y);
+
   timer = setTimeout(startTimer, 10);
   terminate(y);
 }
@@ -40,8 +47,12 @@ function stopTimer() {
 
 function updatePostion(x, y) {
   // console.log(x,y)
-  spaceship.style.left = x + "px";
+
+  if (!abduct) {
+    spaceship.style.left = x + "px";
+  }
   spaceship.style.top = y + "px";
+  ufoFollow(x);
 }
 
 function terminate(y) {
@@ -49,5 +60,23 @@ function terminate(y) {
     // console.log('terminate')
     stopTimer();
     $("#davids_spaceship").remove();
+  }
+}
+
+function ufoFollow(x) {
+  if (abduct) {
+    setTimeout(() => {
+      ufo.animate(
+        {
+          top: "-=100",
+        },
+        100,
+        () => {
+          ufo.remove();
+        }
+      );
+    }, 2400);
+  } else {
+    ufo.css({ left: x - 50 });
   }
 }
