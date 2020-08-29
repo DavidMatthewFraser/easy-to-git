1e493cb9c5313068ced6a89de3603e452fd2a75c
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-08-29 12:50:22 -0700

    formatting js

--- widgets/widgets.js
+++ widgets/widgets.js
@@ -1,11 +1,10 @@
+// comment carousel
 // jquery to hide and toggle the comments until button is clicked
-
 function changeButtonName(button) {
   var currentBtnText = $(button).text();
   var buttonAction = currentBtnText.includes('see') ? 'hide' : 'see';
   $(button).text('Click to ' + buttonAction + ' Comments');
 }
-
 // toggle function
 $('#btn_com').click(function(){
   $('#randSec').toggle('swing', changeButtonName.bind(null, this));
