8442ab90f75e85f476329f7ef708e64b2c83789d
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2020-09-15 21:47:10 -0700

    added weather widget javascript

--- widgets/widgets.js
+++ widgets/widgets.js
@@ -28,4 +28,55 @@ $('#googleSearch').submit((e)=>{
       let searchUrl = `https://www.google.com/search?q=${searchTopic}&oq=${searchTopic}&ie=UTF-8`;
      window.open(searchUrl);
     });
+// weather ninja widget
+// added by Terri Fricker, based on tutorial by NetNina
+const cityForm = document.querySelector('form');
+const display = document.querySelector('.display');
+const details = document.querySelector('.details');
+const image = document.querySelector('.display img');
+const icon = document.querySelector('.icon img');
+
+const updateDisplay = data => {
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+  details.innerHTML = `
+    <h5>${cityDetails.EnglishName}</h5>
+    <div class="condition">${weather.WeatherText}</div>
+    <div class="temperature">
+      <span>${weather.Temperature.Imperial.Value}</span>
+      <span>&deg;F</span>
+    </div>
+  `;
+  let imageSource = null;
+  if(weather.IsDayTime) {
+    imageSource = 'images/day.svg';
+  } else {
+    imageSource = 'images/night.svg';
+  }
+  image.setAttribute('src', imageSource);
+  const iconSource = `images/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSource);
+  if (display.classList.contains('hidden')) {
+    display.classList.remove('hidden');
+  }
+}
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+  console.log(weather);
+  return {
+    cityDetails: cityDetails,
+    weather: weather
+  }
+};
+
+cityForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+  updateCity(city)
+    .then(data => updateDisplay(data))
+    .catch(err => console.log(err));
+})
 
